0938ef4ecc7c43e610c5afecc00cf3ed0db08390
Viktor <kosenkovv89@gmail.com>
2020-09-17 10:52:40 +0300

    babel

---
+++ src/main.js
@@ -0,0 +1,14 @@
+class App {
+
+    run = async (name = 'World') => {
+        console.log(`Hello ${name}`);
+        console.log([1, 2, [2, 3]].flat());
+    };
+}
+
+
+
+const app = new App();
+app.run()
+    .then(() => console.log('done'))
+    .catch(() => console.log('error!'));
